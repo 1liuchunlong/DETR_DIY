d30a74a89a576192f9cc0dccd3f309bb34595aa0
chunchun <2455749429@qq.com>
2024-12-03 23:08:06 +0800

    POS

---
+++ models/backbone.py
@@ -0,0 +1,17 @@
+
+"""
+Backbone modules. Resnet
+"""
+from collections import OrderedDict
+
+import torch
+import torch.nn.functional as F
+import torchvision
+from torch import nn
+from torchvision.models._utils import IntermediateLayerGetter
+from typing import Dict, List
+
+from util.misc import NestedTensor, is_main_process
+
+from .position_encoding import build_position_encoding
+
